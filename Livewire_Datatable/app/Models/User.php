57d5f19b19d63b936a2575f2de580534d222ecfe
alsakib748 <alsakib748@gmail.com>
2025-01-05 23:49:57 +0600

    Livewire Datatable

--- Livewire_Datatable/app/Models/User.php
+++ Livewire_Datatable/app/Models/User.php
@@ -9,35 +9,20 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -45,4 +30,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function scopeSearch($query, $value){
+        $query->where('name','like',"%{$value}%")->orWhere('email','like',"%{$value}%");
+    }
+
 }
